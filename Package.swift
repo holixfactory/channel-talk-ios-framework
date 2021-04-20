b31492273437e8e438aab6013cac03256208e0db
jam <jam@zoyi.co>
2021-04-20 18:33:07 +0900

    test

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIO",
-            url: "https://mobile-static.channel.io/ios/8.4.1/spm-xcframework.zip",
-            checksum: "8eec38a1642ab4ff02e131fca9cd545b376dd41b42005620c7efcaddb193f4ce")
+            url: "https://mobile-static.channel.io/ios/0.0.1/test-xcframework.zip",
+            checksum: "5cede54311b4277ed34985279f26b1de6bf4137cbda677b83d2995de8586a495")
     ]
 )
