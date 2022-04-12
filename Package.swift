2d69702afec8960f92ede282daf4911f9e3ea7bf
Dahoon Kim <salqueng@gmail.com>
2022-04-12 18:13:25 +0900

    xcode 13.3 fix - export as ChannelIOFront

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         ],
     products: [
         .library(
-            name: "ChannelIOSDK",
+            name: "ChannelIOFront",
             targets: ["ChannelIOFront"]),
     ],
     targets: [
